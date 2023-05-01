afa01f29b6c3d4bad4605bf86d68296a0c1115d9
Logue <logue@hotmail.co.jp>
2023-05-01 19:24:20 +0900

    Add z-offset feature. Fixed CVE-2023-2251. Update to TypeScript 5 and tsconfig. Update dependencies.

--- src/geojson-offset.ts
+++ src/geojson-offset.ts
@@ -5,34 +5,38 @@ import type { GeoJSON, Position } from 'geojson';
  * @param geojson - GeoJSON to change
  * @param xOffset - X-coordinate offset
  * @param yOffset - Y-coordinate offset
+ * @param zOffset - Z-coordinate offset
  */
 export const offset = (
   geojson: GeoJSON,
   xOffset: number,
-  yOffset: number
+  yOffset: number,
+  zOffset?: number
 ): GeoJSON => {
   switch (geojson.type) {
     case 'Point':
-      pointOffset(geojson.coordinates, xOffset, yOffset);
+      pointOffset(geojson.coordinates, xOffset, yOffset, zOffset);
       break;
     case 'MultiPoint':
     case 'LineString':
-      lineStringOffset(geojson.coordinates, xOffset, yOffset);
+      lineStringOffset(geojson.coordinates, xOffset, yOffset, zOffset);
       break;
     case 'MultiLineString':
     case 'Polygon':
-      polygonOffset(geojson.coordinates, xOffset, yOffset);
+      polygonOffset(geojson.coordinates, xOffset, yOffset, zOffset);
       break;
     case 'MultiPolygon':
       geojson.coordinates.forEach(polygon => {
-        polygonOffset(polygon, xOffset, yOffset);
+        polygonOffset(polygon, xOffset, yOffset, zOffset);
       });
       break;
     case 'Feature':
-      offset(geojson.geometry, xOffset, yOffset);
+      offset(geojson.geometry, xOffset, yOffset, zOffset);
       break;
     case 'FeatureCollection':
-      geojson.features.forEach(feature => offset(feature, xOffset, yOffset));
+      geojson.features.forEach(feature =>
+        offset(feature, xOffset, yOffset, zOffset)
+      );
       break;
     default:
       throw new Error(`geojson-offset: ${geojson.type} is not supported type.`);
@@ -46,42 +50,59 @@ export const offset = (
  * @param geojson - GeoJSON to change
  * @param xRange - X-coordinate offset range
  * @param yRange - Y-coordinate offset range
+ * @param zRange - Z-coordinate offset range
  */
 export const randomOffset = (
   geojson: GeoJSON,
   xRange: Position,
-  yRange: Position
+  yRange: Position,
+  zRange?: Position
 ): GeoJSON => {
   const xOffset =
     (xRange[0] ?? 0) + ((xRange[1] ?? 0) - (xRange[0] ?? 0)) * Math.random();
   const yOffset =
     (yRange[0] ?? 0) + ((yRange[1] ?? 0) - (yRange[0] ?? 0)) * Math.random();
+  const zOffset =
+    zRange !== undefined
+      ? (zRange[0] ?? 0) + ((zRange[1] ?? 0) - (zRange[0] ?? 0)) * Math.random()
+      : undefined;
 
-  return offset(geojson, xOffset, yOffset);
+  return offset(geojson, xOffset, yOffset, zOffset);
 };
 
 /**
  * @param coordinates - Current position
  * @param x - Offset X
  * @param y - Offset Y
+ * @param z - Offset Z
  */
-const pointOffset = (coordinates: Position, x: number, y: number): void => {
+const pointOffset = (
+  coordinates: Position,
+  x: number,
+  y: number,
+  z?: number
+): void => {
   coordinates[0] += x;
   coordinates[1] += y;
+  if (z !== undefined) {
+    coordinates[2] += z;
+  }
 };
 
 /**
  * @param coordinates - LineString coordinates
  * @param x - Offset X
  * @param y - Offset Y
+ *  @param z - Offset Z
  */
 const lineStringOffset = (
   coordinates: Position[],
   x: number,
-  y: number
+  y: number,
+  z?: number
 ): void => {
   coordinates.forEach(point => {
-    pointOffset(point, x, y);
+    pointOffset(point, x, y, z);
   });
 };
 
@@ -93,9 +114,10 @@ const lineStringOffset = (
 const polygonOffset = (
   coordinates: Position[][],
   x: number,
-  y: number
+  y: number,
+  z?: number
 ): void => {
   coordinates.forEach(line => {
-    lineStringOffset(line, x, y);
+    lineStringOffset(line, x, y, z);
   });
 };
